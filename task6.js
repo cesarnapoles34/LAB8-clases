31a8c075a805e7d239b3f336455b458092da3b53
Cesar Napoles <cesarnapoles34@gmail.com>
2026-09-25 15:14:46 -0600

    Tarea 6 extra: party

--- task6.js
+++ task6.js
@@ -1,7 +1,33 @@
 export class Player {
-    constructor() {
-      
+    constructor(name, level) {
+        this.name = name;
+        this.level = level;
+        this.xp = 0;
+        this.xpToLevel = 100;
+        this.party = [];
     }
-  
-    
-  }
+
+    info() {
+        return `${this.name} has reached Level ${this.level}!`;
+    }
+
+    levelUp() {
+        this.level++;
+    }
+
+    gainXp(amount) {
+        this.xp += amount;
+        while (this.xp >= this.xpToLevel) {
+            this.xp -= this.xpToLevel;
+            this.levelUp();
+        }
+    }
+
+    addToParty(player) {
+        this.party.push(player);
+    }
+
+    removeFromParty(player) {
+        this.party = this.party.filter(member => member !== player);
+    }
+}

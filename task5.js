ef0cfb5433c3dddf2d8d982fdf9a5b04ea37f0f7
Cesar Napoles <cesarnapoles34@gmail.com>
2026-09-25 15:07:55 -0600

    Tarea 5 extra: experiencia

--- task5.js
+++ task5.js
@@ -1,7 +1,24 @@
 export class Player {
-    constructor() {
-      
+    constructor(name, level) {
+        this.name = name;
+        this.level = level;
+        this.xp = 0;
+        this.xpToLevel = 100;
     }
-  
-    
-  }
+
+    info() {
+        return `${this.name} has reached Level ${this.level}!`;
+    }
+
+    levelUp() {
+        this.level++;
+    }
+
+    gainXp(amount) {
+        this.xp += amount;
+        while (this.xp >= this.xpToLevel) {
+            this.xp -= this.xpToLevel;
+            this.levelUp();
+        }
+    }
+}
